ead2bb63ea5cbc5605a038f360cda65f13d634fd
ShashithaU <udeshshashitha@gmail.com>
2025-04-12 00:05:32 +0530

    Courses dto  class was implemented

--- src/main/java/com/bodima/project_lms/dto/CourseDto.java
+++ src/main/java/com/bodima/project_lms/dto/CourseDto.java
@@ -1,4 +1,34 @@
 package com.bodima.project_lms.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CourseDto {
+
+    @Id
+    private Integer courseId;
+    private String title;
+    private String description;
+    private String instructor;
+    private String catagoty;
+    private String level; // intermediate,beginner,advanced
+    private Set<String> modules;
+    private double price;
+    private Integer enrollmentLimit;
+    private Date startData;
+    private Date endDate;
+    private String language;
+    private String resources;
+    private String reviewForCourse;
+    private Double rating;
+    private String status;
+
 }
