51c7efdc58401160aed4bf6926a66d3a4e6af207
ShashithaU <udeshshashitha@gmail.com>
2025-04-11 10:51:43 +0530

    initial commit

---
+++ src/main/java/com/bodima/project_lms/ProjectLmsApplication.java
@@ -0,0 +1,13 @@
+package com.bodima.project_lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectLmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bodima/project_lms/ProjectLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bodima.project_lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
