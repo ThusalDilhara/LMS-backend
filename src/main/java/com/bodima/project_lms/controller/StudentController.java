2d526275f154aeee1420df928ad72ddcb2b47cc5
thurunuym <marasinghe.ty@gmail.com>
2025-05-12 16:52:49 +0530

    renamed adminController as StudentController

--- src/main/java/com/bodima/project_lms/controller/StudentController.java
+++ src/main/java/com/bodima/project_lms/controller/StudentController.java
@@ -6,24 +6,27 @@ import com.bodima.project_lms.service.StudentService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/api/admin")
-public class AdminController {
+public class StudentController {
 
     @Autowired
     private StudentService studentService;
 
     @PostMapping("/register-student")
-    @PreAuthorize("hasRole('ADMIN')")
+    // @PreAuthorize("hasRole('ADMIN')")
     public Student registerStudent(@RequestBody Student student) {
         return studentService.registerStudent(student);
     }
 
-    @GetMapping("/get-all")
+
+
+    @GetMapping("/get-all-students")
     @PreAuthorize("hasRole('ADMIN')")
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
