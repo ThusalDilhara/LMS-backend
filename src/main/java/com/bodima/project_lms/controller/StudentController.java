94e8808dc192c3ca98d78ff2743e1a41b411ca2b
thurunuym <marasinghe.ty@gmail.com>
2025-05-16 23:55:11 +0530

    students updating function added

--- src/main/java/com/bodima/project_lms/controller/StudentController.java
+++ src/main/java/com/bodima/project_lms/controller/StudentController.java
@@ -10,9 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
-@RequestMapping("/api/admin")
+@RequestMapping("/api/student")
 public class StudentController {
 
     @Autowired
@@ -27,8 +27,16 @@ public class StudentController {
 
 
     @GetMapping("/get-all-students")
-    @PreAuthorize("hasRole('ADMIN')")
+    // @PreAuthorize("hasRole('ADMIN')")
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
+
+    @PutMapping("/update-student/{id}")
+    // @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Student> updateStudent(@PathVariable String id, @RequestBody Student student) {
+        return ResponseEntity.ok(studentService.updateStudent(id , student));
+    }
+
+
 }
--- src/main/java/com/bodima/project_lms/service/StudentService.java
+++ src/main/java/com/bodima/project_lms/service/StudentService.java
@@ -2,10 +2,14 @@ package com.bodima.project_lms.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.bodima.project_lms.model.Student;
 
 public interface StudentService {
     Student registerStudent(Student student);
 
     List<Student> getAllStudents();
+
+    Student updateStudent(String id, Student student);
 }
