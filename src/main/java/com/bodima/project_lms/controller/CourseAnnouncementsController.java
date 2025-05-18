29d729734d418e887ede166e308739d4b6229424
ShashithaU <udeshshashitha@gmail.com>
2025-05-18 23:19:10 +0530

    bug fix

--- src/main/java/com/bodima/project_lms/controller/CourseAnnouncementsController.java
+++ src/main/java/com/bodima/project_lms/controller/CourseAnnouncementsController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/course-announcements")
 @RequiredArgsConstructor
@@ -16,12 +18,12 @@ public class CourseAnnouncementsController {
 
     private final CourseAnnouncementsService courseAnnouncementsService;
 
-    @PostMapping(value = "/add-announcements",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/add-announcements", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ResponseEntity<String> addAnnouncements(
-            @RequestParam(value = "file",required = false) MultipartFile file,
-            @RequestParam(value = "courseId",required = false) int courseId,
-            @RequestParam(value = "description",required = false) String description,
+            @RequestParam(value = "file", required = false) MultipartFile file,
+            @RequestParam(value = "courseId", required = false) int courseId,
+            @RequestParam(value = "description", required = false) String description,
             @RequestParam(value = "closeDate", required = false) String closeDate,
             @RequestParam(value = "openDate", required = false) String openDate,
             @RequestParam(value = "contentMonth", required = false) String contentMonth,
@@ -33,7 +35,7 @@ public class CourseAnnouncementsController {
 
     ) {
 
-        try{
+        try {
 
             CourseAnnouncements announcement = new CourseAnnouncements();
             announcement.setCourseId(courseId);
@@ -46,11 +48,16 @@ public class CourseAnnouncementsController {
             announcement.setIsNote(isNote);
             announcement.setIsAnnouncement(isAnnouncement);
 
-            courseAnnouncementsService.addAnnouncements(announcement,file);
+            courseAnnouncementsService.addAnnouncements(announcement, file);
             return ResponseEntity.ok("Announcement created successfully with file");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Failed to upload file: " + e.getMessage());
         }
+    }
+
+    @GetMapping("/get-annousements-and-content")
+    public List<CourseAnnouncements> getAnnouementsAndContents(@RequestParam int id){
+        return courseAnnouncementsService.getAnnouementsAndContents(id);
     }
 }
--- src/main/java/com/bodima/project_lms/service/CourseAnnouncementsService.java
+++ src/main/java/com/bodima/project_lms/service/CourseAnnouncementsService.java
@@ -4,7 +4,10 @@ import com.bodima.project_lms.dto.CourseAnnouncements;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface CourseAnnouncementsService {
     void addAnnouncements(CourseAnnouncements courseAnnouncements, MultipartFile file) throws IOException;
+
+    List<CourseAnnouncements> getAnnouementsAndContents(int id);
 }
